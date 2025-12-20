32d81c58ceeb0e3edae2815d89331bdebe6296ce
Francisco Murillo <franciscomurillo@Franciscos-MacBook-Air.local>
2025-12-20 09:54:23 -0600

    week1

---
+++ practice/week1/fundamentals.py
@@ -0,0 +1,52 @@
+# grades = [88, 92, 79, 93, 85,60]
+# # sortGrades = grades.sort();
+
+# for i in range(len(grades)):    
+#     if grades[i] > 60 and grades[i] < 70:
+#         print("The  grade is Novice (", grades[i], ")")
+#     elif grades[i] >= 70 and grades[i] < 80:
+#         print("The grade is Competent (", grades[i], ")")
+#     elif grades[i] >= 80 and grades[i] < 90: 
+#         print("The grade is Proficient (", grades[i], ")")
+#     elif grades[i] >= 90 and grades[i] <= 100:
+#         print("The grade is Expert (", grades[i], ")")
+#     else:
+#         print("The grade is Fail (", grades[i], ")")
+
+
+# def greeting(name):
+#     print("Hello, " + name + "!")
+
+# greeting("Alice")
+
+# # area = base * height / 2
+# def calculate_triangle_area(base,height):
+#     area = base * height / 2
+#     return area
+
+# print(calculate_triangle_area(5,10))
+
+# return the largest number in a list
+# def find_largest_number(numbers):
+#     largest = numbers[0]
+#     print("Initial largest:", largest)
+#     for number in numbers:
+#         if number > largest:
+#             largest = number
+#     return largest  
+
+# print(find_largest_number([3, 5, 2, 8, 1]))  # Output: 8
+
+
+#List   
+# numbers = [1,2,3,4,5,6,]
+
+# numbers.append(7) # add element at the end
+# print(numbers) 
+# numbers.remove(2) # remove element by value
+# print(numbers)
+# numbers.insert(2,10) # insert element at index
+# print(numbers)
+# numbers.pop() # remove last element
+# print(numbers)
+
